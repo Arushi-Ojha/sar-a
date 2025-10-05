616bb17a3012bf81ffbc82cb46dc391167ce2b5a
Arushi-Ojha <arushiojha100@gmail.com>
2025-10-05 22:54:27 +0530

    Initial commit - frontend for SAR-A

--- src/pages/Minecraft.jsx
+++ src/pages/Minecraft.jsx
@@ -4,7 +4,7 @@ import { OrbitControls, useGLTF } from "@react-three/drei";
 
 function MinecraftModel() {
   const group = useRef();
-  const { scene } = useGLTF("./ice_spikes_minecraft/scene.gltf");
+  const { scene } = useGLTF("/ice_spikes_minecraft/scene.gltf");
 
   return <primitive ref={group} object={scene} scale={7} />;
 }
